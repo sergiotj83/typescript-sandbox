daad5e85829bb6126a4a2c6ec000bed5a455e172
Sergio <sergiotj83@hotmail.com>
2025-04-03 11:45:40 +0200

    Subida Laboratorio_02

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+console.log((120 - 18) / 6);
+console.log(17 + 18);
+console.log(
+  "Los demás pagan 17 y yo 35 por los 18 de la bebida y mi parte de comida 17"
+);
